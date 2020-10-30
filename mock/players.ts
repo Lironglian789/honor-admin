6e465b795bd25fed960bba10e19323bbc27c4c11
李容连 <lironglian@ewell.cc>
2020-10-30 14:35:48 +0800

    完成玩家列表接口模拟

---
+++ mock/players.ts
@@ -0,0 +1,56 @@
+import { Response, Request } from 'express'
+import { Hero, Player } from '../src/api/types'
+import { heros } from './heros'
+import faker from 'faker'
+
+faker.locale = 'zh_CN'
+
+// 模拟玩家数据
+const playCount = 100
+const playerList: Player[] = []
+
+for(let i = 0; i < playCount; i++) {
+  playerList.push({
+    id: i,
+    accountname: faker.name.findName(),
+    avatar: faker.image.avatar(),
+    bravepoints: faker.random.number(1000),
+    exp: faker.random.number(100000),
+    level: faker.random.number(30),
+    nickname: faker.name.findName(),
+    rank: faker.random.number(200),
+    wanttoplay: genWantoPlay(),
+    winningstreak: faker.random.number(10)
+  })
+}
+
+// 随机抽取3个想玩的英雄
+function genWantoPlay() {
+  let wanttoplay: Set<Hero> = new Set() // 可以避免重复
+  while(wanttoplay.size < 3) {
+    wanttoplay.add(heros[faker.random.number(9)]) // 0 - 9 随机抽取
+  }
+  return Array.from(wanttoplay)
+}
+
+// 实现路由
+export const getPlayers = (req: Request, res: Response) => {
+  // 从查询的参数中获取分页、过滤关键词等参数
+  const { accountname, page = 1, limit = 10 } = req.params
+  // 根据账号名过滤
+  let mockList = playerList.filter(item => {
+    if (accountname && item.accountname.indexOf(accountname) < 0) {
+      return false
+    }
+    return true
+  })
+
+  const pageList = mockList.filter((item, index)=> index < (limit as number) * (page as number) && index >= (limit as number) * ((page as number) - 1))
+  return res.json({
+    code: 20000,
+    data: {
+      total: mockList.length,
+      items: pageList
+    }
+  })
+}
